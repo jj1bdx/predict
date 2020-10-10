jj1bdx/predict

commit cd21a1ada06fc4e5a7fe992015100eac3a56ce67

passivesock(): fix listen() fail decision code

predict.c
@@ -2030,7 +2030,7 @@ int passivesock(char *service, char *protocol, int qlen) {
     exit(-1);
   }
 
-  if ((type = SOCK_STREAM && listen(sd, qlen)) < 0) {
+  if ((type = SOCK_STREAM) && (listen(sd, qlen) < 0)) {
     bailout("Listen fail");
     exit(-1);
   }

commit bee6b284b293034cf51c0b123cfe9e5976bd7aaa

passivesock(): rollback listen() fix
* Roll back the bug introduced in cd21a1ada06fc4e5a7fe992015100eac3a56ce67

predict.c
@@ -2035,7 +2035,7 @@ int passivesock(char *service, char *protocol, int qlen) {
     exit(-1);
   }
 
-  if ((type = SOCK_STREAM) && (listen(sd, qlen) < 0)) {
+  if (((type = SOCK_STREAM) && listen(sd, qlen)) < 0) {
     bailout("Listen fail");
     exit(-1);
   }